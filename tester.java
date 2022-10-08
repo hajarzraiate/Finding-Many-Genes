19aa84b2e88d2d922eb127a4c15cda9072b582e5
hajarzraiate <97097632+hajarzraiate@users.noreply.github.com>
2022-10-08 13:07:16 +0100

    Create tester.java

---
+++ tester.java
@@ -0,0 +1,22 @@
+public class Tester {
+    public void findAbc(String input){
+       int index = input.indexOf("abc");
+       while (true){
+           if (index == -1 || index >= input.length() - 3){
+               break;
+           }
+           System.out.println(index);
+           String found = input.substring(index+1, index+4);
+           System.out.println(found);
+           index = index = input.indexOf("abc",index+3);
+           System.out.println("index after updating " + index);
+       }
+    }
+   
+
+
+   public void test(){
+       //findAbc("abcd");
+       findAbc("abcdkfjsksioehgjfhsdjfhksdfhuwabcabcajfieowj");
+   }
+    }
